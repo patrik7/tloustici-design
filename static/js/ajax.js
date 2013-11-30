76b1bbf0fd893c07585ee41ba591bacedc570371
Patrik Beck <patrik.beck@celoxica.com>
2013-11-30 16:56:58 +0100

    ajax code separated, pagination added to search

---
+++ static/js/ajax.js
@@ -0,0 +1,86 @@
+
+var loadDataMore = function() {}
+var root = '';
+var ajax_get = {};
+
+function addData(entries) {
+  //detect ID offset, and last_end
+  var offset = 0;
+  var last_end = 0;
+
+  childs = document.getElementById('data').childNodes;
+  for (var i = 0; i < childs.length; i++) {
+    var e = childs[i];
+    if (e.id == '') offset += 1;
+    else {
+      last_end = parseInt(e.id.substring(4, e.id.length));
+    }
+  }
+
+  for (var i = 0; i < entries.length; i++) {
+    if (entries[i].level == 3) last_end = i;
+
+    render(offset + i, entries[i], last_end);
+  }
+}
+
+function setupPagination(pagination) {
+  if (pagination && pagination.has_next) {
+    loadDataMore = function() {
+      setLoading('pagination');
+      
+      $.getJSON(ajax_url + root, $.extend({}, ajax_get, {'page': pagination.page_number + 1})).done(function(data) {
+        var entries = data.entries;
+        var pagination = data.pagination;
+
+        addData(entries);
+        setupPagination(data.pagination);
+
+        $('#pagination').html('');
+      });
+
+    }
+  } else {
+    loadDataMore = function() {}
+  }
+}
+
+function loadData()
+{
+  setLoading('data');
+  setupPagination(null);
+
+  $.getJSON(ajax_url + root
+
+  ,ajax_get).done(function(data) {
+      $('#data').html('');
+
+      var entries = data.entries;
+      var pagination = data.pagination;
+
+      addData(entries);
+      setupPagination(pagination);
+    })
+    .error(function() {
+      $('#data').html('Error loading data');
+    });
+}
+
+function isScrolledIntoView(elem)
+{
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height();
+
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+
+  return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom)
+    && (elemBottom <= docViewBottom) &&  (elemTop >= docViewTop) );
+}
+
+function scrollFunction() {
+  if (isScrolledIntoView('#pagination')) {
+    loadDataMore();
+  }
+}
+
