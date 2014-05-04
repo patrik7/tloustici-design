4e90bf621f5e7ca16abc20fe7ff4eac7a329686c
Patrik Beck <patrik.beck@celoxica.com>
2014-05-04 09:21:57 +0200

    pre commit script updated

--- pre-commit-test.sh
+++ pre-commit-test.sh
@@ -3,6 +3,9 @@
 SOURCEBASE=/home/pbeck/workspace/Tloustici/src
 LNGFILE='/home/pbeck/workspace/Tloustici/tloustici-design/locale/cs/LC_MESSAGES/django.po'
 
+#make backup
+cp $LNGFILE /tmp
+
 git stash -q --keep-index
 
 cd $SOURCEBASE
@@ -13,6 +16,12 @@ grep fuzzy -A 2 $LNGFILE
 RESULT=$?
 
 git stash pop -q
+
+d=`diff $LNGFILE /tmp/${LNGFILE##*/} | wc -l`
+if [ "$d" == "2" ]; then
+	#no change besides the date
+	cp /tmp/${LNGFILE##*/} $LNGFILE
+fi
 
 [ $RESULT -ne 0 ] && exit 0
 
