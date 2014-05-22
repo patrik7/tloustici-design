a01679b2df5154f08968a4632e8978520c524d5f
Patrik Beck <patrik.beck@celoxica.com>
2014-05-22 07:56:41 +0200

    artile preview tweaking

--- pre-commit-test.sh
+++ pre-commit-test.sh
@@ -6,21 +6,23 @@ LNGFILE='/home/pbeck/workspace/Tloustici/tloustici-design/locale/cs/LC_MESSAGES/
 #make backup
 cp $LNGFILE /tmp
 
-git stash -q --keep-index
+backup=$?
+if [ "$backup" == "0" ]; then
+	git stash -q --keep-index
 
-cd $SOURCEBASE
-django-admin makemessages -s -a
-cd -
+	cd $SOURCEBASE
+	django-admin makemessages -s -a
+	cd -
 
-grep fuzzy -A 2 $LNGFILE
-RESULT=$?
+	grep fuzzy -A 2 $LNGFILE
+	RESULT=$?
 
-git stash pop -q
+	git stash pop -q
 
-d=`diff $LNGFILE /tmp/${LNGFILE##*/} | wc -l`
-if [ "$d" == "4" ]; then
-	#no change besides the date
-	cp /tmp/${LNGFILE##*/} $LNGFILE
+	cp /tmp/django.po $LNGFILE
+else
+	echo "Cannot create backup"
+	exit 1
 fi
 
 [ $RESULT -ne 0 ] && exit 0
